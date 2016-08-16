8f4a3974e4c8ee298c1959734eb18c6fbc3843fe
sami4thewin <samiomarmohammed@gmail.com>
2016-08-16 11:36:11 -0400

    Got rid of bug

--- lib/z_auto_parts.rb
+++ lib/z_auto_parts.rb
@@ -2,9 +2,9 @@ require 'nokogiri'
 require 'pry'
 require 'open-uri'
 
-# require "./z_auto_parts/version"
+require_relative "./z_auto_parts/version"
 require_relative './z_auto_parts/cli'
-require_relative './z_auto_parts/recipe'
+require_relative './z_auto_parts/deal'
 
 # module ZAutoParts
 #   # Your code goes here...
